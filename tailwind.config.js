947f9c123e3d0d53acb0ecf5a5ad44fda6e9376c
Artem Timofeev <timofeev.artem.work@gmail.com>
2022-09-01 19:21:19 +0400

    finish task

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                deepPurple: '#7840B0',
+                deepDark: '#25262D',
+                lightGreen: '#D7EBD3',
+                deepGreen: '#5BC044',
+                lightGray: '#E8E8E8',
+                whiteSmoke: '#F5F5F5',
+                darkGray: '#E3E7EE',
+                deepRed: '#EE5B5B',
+                lightRed: '#FFD8D8',
+                deepBlue: '#5E77F9',
+                lightBlue: '#DFE4FE'
+            }
+        },
+    },
+    plugins: [],
+}
